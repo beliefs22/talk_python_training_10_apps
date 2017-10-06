52810cb69f2d660824d5bef0ef484d6a3fb9207d
beliefs22-work <beliefs22@gmail.com>
2017-10-06 16:16:42 -0400

    Added csvs to git ignore added doc string to weather clients get_token.py file

--- 05_weather_client/get_token.py
+++ 05_weather_client/get_token.py
@@ -1,4 +1,8 @@
 def get_token():
+    """
+    Gets weather underground api token from api_token.txt file
+    :return: api token
+    """
 
     with open('api_token.txt', 'r') as token_file:
         my_token = token_file.readline().rstrip()
